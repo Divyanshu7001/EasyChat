eb2356a91992b1d4d3014e9f94c24cec4bb9780d
Divyanshu Verma <153366809+Divyanshu7001@users.noreply.github.com>
2024-10-30 00:23:36 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["easychat-3iur.onrender.com"],
+    domains: ["easychat-3iur.onrender.com"], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "easychat-3iur.onrender.com",
+        port: "", 
+        pathname: "/uploads/images/**", 
+      },
+    ],
   },
 };
 
